ebdaee0893afccffbef4c813840acae36aae3856
J Brennan <J_Brennan@me.com>
2021-03-19 14:40:01 -0400

    finished but needs to be styled

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,33 @@
-alert("hello");
 
-//fetching spoonacular for wines
-fetch(
-  "https://api.spoonacular.com/food/wine/pairing?food=clam+chowder&apiKey=9175773144fc417eb84578b92bed4dd9"
-)
-  .then((blob) => {
-    return blob.json();
-    console.log(blob);
-  })
-  .then((response) => {
-    console.log(response);
-  });
-//fetching recipepuppy for meal ideas
-fetch("http://www.recipepuppy.com/api/?i=clams,milk")
-  .then((blob) => {
-    return blob.json();
-  })
-  .then((response) => {
-    console.log(response);
-  });
+
+// ingredients
+// getIngredients functions takes the ingredient input and adds to the list of ingredients the sends the concatinated string of ingredients to the api call recipuppy
+var ingredientBtn = document.querySelector("#ingredientButton");
+var ingredientList = document.getElementById("#ingredientList");
+var ingredientInput = document.querySelector("#ingredientInput");
+var ingredientString = "";
+var recipeBtn = document.querySelector("#recipeButton");
+
+// function getIngredients() {
+ingredientBtn.addEventListener("click", function () {
+  ingredientString += ingredientInput.value + ",";
+  var listItem = document.createElement('li');
+  listItem.innerHTML = ingredientInput.value;
+  ingredientList.appendChild(listItem);
+  ingredientInput.value = "";
+  console.log(ingredientString);
+})
+
+recipeBtn.addEventListener("click", function () {
+  // this removes all child elements from the ingredient list
+  const removeChildren = (parent) => {
+    while (parent.lastChild) {
+      parent.removeChild(parent.lastChild);
+    }
+  };
+
+  // remove all child nodes
+  removeChildren(ingredientList);
+
+
+})
