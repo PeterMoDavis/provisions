d514e768df3fcceffe940224bcf50874b0b10546
legit-jb <j_brennan@me.com>
2021-12-05 14:48:12 -0500

    changed video link

--- assets/js/index.js
+++ assets/js/index.js
@@ -394,7 +394,7 @@ function onYouTubeIframeAPIReady() {
   player = new YT.Player("player", {
     height: "390",
     width: "640",
-    videoId: "H0B0Xg80RGw",
+    videoId: "YS3YsNShyBo",
     playerVars: {
       controls: 0,
       autoplay: 1,
